19e743456a3a4e89c3d92ce6297672303095263c
Adam S <adam.sarli@gmail.com>
2019-04-15 12:22:18 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_genre.rb
+++ db/migrate/008_add_genre.rb
@@ -1,5 +1,7 @@
 class AddGenre < ActiveRecord::Migration[4.2]
   def change 
     add_column(:shows, :genre, :string)
+    add_column(:shows, :day, :string)
+    add_column(:shows, :season, :string)
   end 
 end 
