1d15ea2af440308c4d8144314f53c448d2999fa2
Adam S <adam.sarli@gmail.com>
2019-04-15 12:24:21 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
+  belongs_to :show
+  belongs_to :actor 
+  
+  def say_that_thing_you_say 
+    "#{name} always says: #{catchphrase}"
+  end 
+  
+  # def build_show(attributes)
+  #   Show.new(attributes)
+  # end 
   
 end
